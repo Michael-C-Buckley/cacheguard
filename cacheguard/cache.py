eb45df33c2cdefa9eb09983dd23ac2bd39e37dca
Michael Buckley <michaelcbuckley@proton.me>
2025-05-16 09:28:03 +0000

    Formatted

--- cacheguard/cache.py
+++ cacheguard/cache.py
@@ -2,8 +2,10 @@ from orjson import dumps, loads
 from sopsy import Sops, SopsyInOutType
 from os import path
 
+
 class Cache:
     """Mechanism for sealing and protecting a dataset at rest for commiting to git"""
+
     def __init__(self, sops_file: str, file_type: SopsyInOutType):
         self.sops_file = sops_file
         self.file_type = file_type
@@ -17,7 +19,7 @@ class Cache:
 
         # Dumnmy file creation, or else sops will throw exception
         if path.exists(sops_file):
-            created = False 
+            created = False
         else:
             created = True
             with open(sops_file, "a"):
@@ -31,14 +33,12 @@ class Cache:
         if not created:
             self._load()
 
-
     def _load(self):
         """Unseal the dataset"""
         raw_string = self.sops_reader.decrypt(to_dict=False)
         assert type(raw_string) == bytes
         decoded_string = raw_string.decode()
         self.data = loads(decoded_string)
-
 
     def _save(self):
         """Write the dataset to the encrypted at-rest state"""
@@ -51,7 +51,7 @@ class Cache:
     def add(self, entry: dict):
         """Add a new entry"""
         self.data = {**self.data, **entry}
-        
+
 
 # Local pre-release testing
 if __name__ == "__main__":
