f33076d9c849e3d2c75064eb08e6f8072ca1b547
Michael Buckley <michaelcbuckley@proton.me>
2025-11-06 19:23:45 -0500

    Minor changes to reduce lint and type issues

--- cacheguard/key_cache.py
+++ cacheguard/key_cache.py
@@ -14,12 +14,12 @@ class KeyCache(BaseCache):
         sops_path: str,
         age_pubkeys: list[str] = [],
         pgp_fingerprints: list[str] = [],
-    ):
+    ) -> None:
         super().__init__(sops_path, age_pubkeys, pgp_fingerprints)
         if not self.data:
             self.data = {}
 
-    def load(self) -> dict:
+    def load(self)  -> dict:  # type: ignore[override]
         """Handle the data for key-values by loading with JSON"""
         if obtained_data := super().load():
             self.data = loads(obtained_data)
@@ -27,22 +27,22 @@ class KeyCache(BaseCache):
             self.data = {}
         return self.data
 
-    def save(self):
+    def save(self, *args, **kwargs) -> None:
         """Write the dataset to the encrypted at-rest state"""
         converted_string = dumps(self.data)
         super().save(converted_string)
 
-    def add(self, entry: dict):
+    def add(self, entry: dict) -> None:
         """Add new entries"""
         self.data = {**self.data, **entry}
 
-    def load_env_var(self, env_var):
+    def load_env_var(self, env_var) -> None:
         """Load a key-value pair into the environment from the cache"""
         if not self.data.get(env_var):
             raise KeyError("Key does not exist in Key Cache")
         environ[env_var] = self.data[env_var]
 
-    def deploy(self):
+    def deploy(self) -> None:
         """Load every key-value pair in this cache into the environment"""
         for key in self.data.keys():
             self.load_env_var(key)
